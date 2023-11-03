063fc65720fb527a53ba5ad99eb375bcd0e0110b
e7d <michael@e7d.io>
2023-11-03 18:12:26 +0100

    cancellable container run

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ DIR=$(realpath $(dirname "$0"))
 function to_docker_tags_param() {
     local VERSION=$1
     local ADDITIONAL_TAG=$2
+
     local TAGS="--tag e7db/geekbench:${VERSION} --tag e7db/geekbench:${VERSION%.*} --tag e7db/geekbench:${VERSION%.*.*}"
+
     if [ ! -z "${ADDITIONAL_TAG}" ]; then
         TAGS="${TAGS} --tag e7db/geekbench:${ADDITIONAL_TAG}"
     fi
@@ -14,24 +16,25 @@ function to_docker_tags_param() {
 
 function build_image() {
     local VERSION=$1
-    local MAJOR_VERSION=${VERSION%.*.*}
-    local FOLDER=geekbench${MAJOR_VERSION}
     local PLATFORMS=$2
     local ADDITIONAL_TAG=$3
+
+    local MAJOR_VERSION=${VERSION%.*.*}
     local TAGS=$(to_docker_tags_param ${VERSION} ${ADDITIONAL_TAG})
-    mkdir -p ${DIR}/${FOLDER}/build ${DIR}/${FOLDER}/cache
+
+    mkdir -p ${DIR}/build/geekbench${MAJOR_VERSION} ${DIR}/cache/geekbench${MAJOR_VERSION}
+
     docker buildx create \
         --name e7db-geekbench${MAJOR_VERSION} \
         --platform ${PLATFORMS} \
         --use
     docker buildx build \
-        --cache-from type=local,src=${DIR}/${FOLDER}/cache \
-        --cache-to type=local,dest=${DIR}/${FOLDER}/cache \
+        --cache-from type=local,src=${DIR}/cache/geekbench${MAJOR_VERSION} \
+        --cache-to type=local,dest=${DIR}/cache/geekbench${MAJOR_VERSION} \
         --file ${DIR}/Dockerfile \
-        --metadata-file ${DIR}/${FOLDER}/build/metadata.json \
+        --metadata-file ${DIR}/build/geekbench${MAJOR_VERSION}/metadata.json \
         --output type=registry \
         --platform ${PLATFORMS} \
-        --build-arg FOLDER=${FOLDER} \
         --build-arg VERSION=${VERSION} \
         ${TAGS} \
         ${DIR}
@@ -40,16 +43,20 @@ function build_image() {
 
 VERSIONS=($(echo "$(curl -s https://www.geekbench.com/preview/)" | grep -oE 'Geekbench-[0-9]+\.[0-9]+\.[0-9]+' | awk -F'-' '{print $2}' | sort -u))
 SORTED_VERSIONS=($(printf "%s\n" "${VERSIONS[@]}" | tr ' ' '\n' | sort -t. -k1,1n -k2,2n -k3,3n))
+
 for VERSION in "${SORTED_VERSIONS[@]}"; do
     MAJOR_VERSION=${VERSION%.*.*}
+
     if [ $MAJOR_VERSION -lt 6 ]; then
         PLATFORMS="linux/amd64,linux/arm64/v8,linux/arm/v7"
     else
         PLATFORMS="linux/amd64,linux/arm64/v8"
     fi
+
     unset ADDITIONAL_TAG
     if [ $VERSION == "${SORTED_VERSIONS[-1]}" ]; then
         ADDITIONAL_TAG="latest"
     fi
+
     build_image ${VERSION} ${PLATFORMS} ${ADDITIONAL_TAG}
 done
--- setup.sh
+++ setup.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
+if [ -z $VERSION ]; then
+    echo "VERSION is not set"
+    exit 1
+fi
+
 function extract_arm() {
     tar -xvf /tmp/Geekbench-${VERSION}-LinuxARMPreview.tar.gz -C /tmp
     rm -rf /tmp/Geekbench-${VERSION}-LinuxARMPreview.tar.gz
-    mv /tmp/Geekbench-${VERSION}-LinuxARMPreview /tmp/geekbench
-    rm /tmp/geekbench/geekbench5
+    mv /tmp/Geekbench-${VERSION}-LinuxARMPreview /opt/geekbench
+    rm /opt/geekbench/geekbench${MAJOR_VERSION}
 }
 
 function extract_x86() {
     tar -xvf /tmp/Geekbench-${VERSION}-Linux.tar.gz -C /tmp
     rm -rf /tmp/Geekbench-${VERSION}-Linux.tar.gz
-    mv /tmp/Geekbench-${VERSION}-Linux /tmp/geekbench
-    rm /tmp/geekbench/geekbench5
+    mv /tmp/Geekbench-${VERSION}-Linux /opt/geekbench
+    rm /opt/geekbench/geekbench${MAJOR_VERSION}
 }
 
+MAJOR_VERSION=${VERSION%.*.*}
 if [ $(uname -m) == "armv7l" ]; then
     extract_arm
-    mv /tmp/geekbench/geekbench_armv7 /tmp/geekbench/geekbench
+    EXECUTABLE=geekbench_armv7
 elif [ $(uname -m) == "aarch64" ]; then
     extract_arm
-    mv /tmp/geekbench/geekbench_aarch64 /tmp/geekbench/geekbench
+    EXECUTABLE=geekbench_aarch64
 elif [ $(uname -m) == "x86_64" ]; then
     extract_x86
-    mv /tmp/geekbench/geekbench_x86_64 /tmp/geekbench/geekbench
+    EXECUTABLE=geekbench_x86_64
 else
     echo "Unsupported architecture"
     exit 1
 fi
 
-mv /tmp/geekbench /opt/geekbench/
+echo "#!/bin/bash
+/opt/geekbench/${EXECUTABLE}" >/opt/geekbench/geekbench
+chmod +x /opt/geekbench/geekbench
